3fa5dcbef7e7cca377f5e9711f24ec0676999896
gon17 <gon17@Portatil-de-Izuz>
2020-04-10 19:25:43 +0200

    subida con boostrap

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="icon" type="image/png" href="...">  
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     </head>
     <body>
         <?php
         // put your code here
         ?>
+        
+        
+                <!-- Scripts -->
+        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.scrolly.min.js" type="text/javascript"></script>
+        <script src="assets/js/browser.min.js" type="text/javascript"></script>
+        <script src="assets/js/breakpoints.min.js" type="text/javascript"></script>
+        <script src="assets/js/util.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+        
+        
     </body>
 </html>
