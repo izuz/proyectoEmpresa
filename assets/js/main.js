c57c0e5eb07a2f42ae306efb9200132badd27c71
gon17 <gon17@Portatil-de-Izuz>
2020-04-10 20:50:24 +0200

    ESTRUCTURA BÁSICA

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,70 @@
     $('.scrolly').scrolly();
 
 })(jQuery);
+
+$('.navbar-nav li a').on('click', function () {
+    if ($(this).attr('id') !== "navbarDropdown") {
+        $('.navbar-collapse').collapse('hide');
+
+    }
+});
+
+$('#veInicio').click(function (e) {
+    e.preventDefault();
+
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html').animate({scrollTop: $('#inicio').offset().top}, 500, 'swing');
+
+
+    return false;
+
+});
+
+$('#veEmpresa').click(function (e) {
+    e.preventDefault();
+
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html').animate({scrollTop: $('#empresa').offset().top}, 500, 'swing');
+
+
+    return false;
+
+});
+
+$('#veProyectos').click(function (e) {
+    e.preventDefault();
+
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html').animate({scrollTop: $('#proyectos').offset().top}, 500, 'swing');
+
+
+    return false;
+
+});
+
+$('#veEquipo').click(function (e) {
+    e.preventDefault();
+
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html').animate({scrollTop: $('#equipo').offset().top}, 500, 'swing');
+
+
+    return false;
+
+});
+
+$('#veContacto').click(function (e) {
+    e.preventDefault();
+
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html').animate({scrollTop: $('#contacto').offset().top}, 500, 'swing');
+
+
+    return false;
+
+});
--- index.php
+++ index.php
@@ -1,19 +1,167 @@
 <!DOCTYPE html>
-<html>
+<html id="inicio">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>LIF DESARROLLADORES</title>
         <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="icon" type="image/png" href="...">  
         <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
 
+    <body>
+
+        <!-- NAV -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="menu">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="index.html" id="contenedorLogo"><img src="" id="logo">LOGO / NOMBRE EMPRESA</a>           
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto" id="contenido_menu">
+                        <li class="nav-item active">
+                            <a id="veInicio" class="nav-link" href="#inicio"><i class="fa fa-home">INICIO</i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="veEmpresa" class="nav-link" href="#empresa"><i class="fa fa-sitemap">EMPRESA</i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="veProyectos" class="nav-link" href="#proyectos" ><i class="fa fa-camera">PROYECTOS</i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="veEquipo" class="nav-link" href="#equipo"><i class="fa fa-user">EQUIPO</i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="veContacto" class="nav-link" href="#contacto"><i class="fa fa-envelope">CONTACTO</i></a>
+                        </li>
+                    </ul>
+                </div>    
+            </div>
+        </nav>
+
+        <br>
+        <br>
+
+        <!-- EMPRESA -->
+        <section class="main">
+            <header id="empresa">
+                <div class="container">
+                    <br>
+                    <h3 id="titulo_empresa">NUESTRAS EMPRESA</h3>
+                    <br>
+                    <p id="texto_empresa">AQUÍ VA LA DESCRIPCION DE LA EMPRESA</p>
+                </div>
+            </header>
+        </section>
+
+        <br>
+        <br>
+        <br>
+
+        <!-- PROYECTOS -->
+        <section id="proyectos">
+            <header>
+                <div class="container">
+                    <br>
+                    <h3 id="titulo_proyectos">NUESTROS PROYECTOS</h3>
+                    <br>
+                    <p id="texto_proyectos">GALERÍA CON LOS PROYECTOS</p>
+                </div>
+            </header>
+        </section>
+
+        <br>
+        <br>
+        <br>
+
+        <!-- EQUIPO -->
+        <section  id="equipo">
+            <header>
+                <div class="container">
+                    <br>
+                    <h3 id="titulo_equipo">NUESTRO EQUIPO</h3>
+                    <br>
+                    <div class="card-deck">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">GONZALO IZUZQUIZA</h5>
+                                <p class="card-text">Estudios: Técnico en desarrollo de aplicaciones multiplataforma y estudiante de ingeniería informática</p>
+                                <p class="card-text">Edad: 22</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">ALEJANDRO LAGO</h5>
+                                <p class="card-text">Estudios: Técnico en desarrollo de aplicaciones multiplataforma y estudiante de ingeniería informática</p>
+                                <p class="card-text">Edad: 23</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">CARLOS FUENTES</h5>
+                                <p class="card-text">Estudios: Técnico en desarrollo de aplicaciones multiplataforma y estudiante de python</p>
+                                <p class="card-text">Edad: 105</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </section>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <!-- CONTACTO -->
+        <section id="contacto">
+            <header>
+                <div class="container">
+                    <br>
+                    <h3 id="titulo_contacto">CONTÁCTANOS</h3>
+                    <br>
+                    <p id="texto_contacto">AQUI FORMULARIO DE CONTACTO</p>
+                </div>
+            </header>
+
+            <footer id="footer">
+                <div class="container">
+                    <ul class="icons">
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+                        <li><a href="#" class="icon fa-whatsapp"><span class="label">WhatsApp</span></a></li>
+                    </ul>
+                </div>
+                <div class="copyright" >
+                    &copy; LIF DESARROLLADORES
+                </div>
+            </footer>
+
+        </section>
+
+        <!-- REDES SOCIALES -->
+        <!--<footer id="footer">
+            <div class="container">
+                <ul class="icons">
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+                    <li><a href="#" class="icon fa-whatsapp"><span class="label">WhatsApp</span></a></li>
+                </ul>
+            </div>
+            <div class="copyright" >
+                &copy; LIF DESARROLLADORES
+            </div>
+        </footer>-->
 
         <!-- Scripts -->
         <script src="assets/js/jquery.min.js" type="text/javascript"></script>
@@ -23,7 +171,6 @@
         <script src="assets/js/breakpoints.min.js" type="text/javascript"></script>
         <script src="assets/js/util.js" type="text/javascript"></script>
         <script src="assets/js/main.js" type="text/javascript"></script>
-
 
     </body>
 </html>
